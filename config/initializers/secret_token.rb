2d1a68819b37c976ba439aeee0d4b6a8706f0eed
Daniel Lundin <daniel.lundin@scilifelab.se>
2012-04-18 14:08:41 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Environmentmicrobedb::Application.config.secret_token = '62791c645eb4cdb6ba3e6d68e607b7118d29260a9f06dcd07f75d6b4c8b7bd063fb7d97767ec7c28d8300569a37ee65e5f7402a3b376a988fbfd272c8a7f23c0'
