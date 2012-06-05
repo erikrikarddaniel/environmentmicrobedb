0e1729922da3ac7cb1738d2d4116d72e01fbf85b
Brynjar Smari Bjarnason <binni@binnisb.com>
2012-06-05 20:44:54 +0200

    Adding table information to the model, schema and test.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id         :integer         not null, primary key
+#  property   :integer
+#  value      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class Property < ActiveRecord::Base
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id         :integer         not null, primary key
+#  property   :integer
+#  value      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Property do
