45afad113a044899c083cd9b0c0389bc23a878af
Brynjar Smari Bjarnason <binni@binnisb.com>
2012-07-05 18:22:20 +0200

    added conversion validation to properties

--- app/models/global_constants.rb
+++ app/models/global_constants.rb
@@ -1,12 +1,12 @@
 module GlobalConstants
   # also notice the call to 'freeze'
-  PROPERTY_TYPE = {"integer" => {can_convert?: (lambda {|value| Integer(value) rescue "Can't Convert"}) }, 
-                   "string" => {can_convert?: (lambda {|value| String(value) rescue "Cant Convert"}) },
-                   "datetime" => {can_convert?: (lambda {|value| value.to_datetime rescue "Cant Convert"}) }, 
-                   "float" => {can_convert?: (lambda {|value| Float(value) rescue "Cant Convert"}) },
-                   "date" => {can_convert?: (lambda {|value| value.to_datetime.to_date rescue "Cant Convert"}) }, 
-                   "time" => {can_convert?: (lambda {|value| value.to_datetime.to_time rescue "Cant Convert"}) },
-                   "bool" => {can_convert?: (lambda {|value| to_bool(value) rescue "Can't Convert"}) } 
+  PROPERTY_TYPE = {"integer" => {can_convert?: (lambda {|value| Integer(value) rescue :error}) }, 
+                   "string" => {can_convert?: (lambda {|value| String(value) rescue :error}) },
+                   "datetime" => {can_convert?: (lambda {|value| value.to_datetime rescue :error}) }, 
+                   "float" => {can_convert?: (lambda {|value| Float(value) rescue :error}) },
+                   "date" => {can_convert?: (lambda {|value| value.to_datetime.to_date rescue :error}) }, 
+                   "time" => {can_convert?: (lambda {|value| value.to_datetime.to_time rescue :error}) },
+                   "bool" => {can_convert?: (lambda {|value| to_bool(value) rescue :error}) } 
                    }.freeze
   def self.to_bool(value)
     return true if value == true || value =~ (/(true|t|yes|y|1)$/i)
--- app/models/project_property.rb
+++ app/models/project_property.rb
@@ -17,4 +17,12 @@ class ProjectProperty < ActiveRecord::Base
   validates_presence_of :name, :value, :project_id, :datatype
   validates :datatype,  :inclusion => { :in => GlobalConstants::PROPERTY_TYPE.keys,
     :message => "%{value} is not a valid data type" }
+  validate :convert_value
+  def convert_value
+    if GlobalConstants::PROPERTY_TYPE[self.datatype][:can_convert?].call(self.value) == :error
+      errors.add(:error,"can't convert to #{self.datatype}")
+    else
+      GlobalConstants::PROPERTY_TYPE[self.datatype][:can_convert?].call(self.value)
+    end
+  end
 end
