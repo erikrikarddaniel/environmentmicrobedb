46f117742d2f45bd8d3f747ae8c83abb1f610c05
Daniel Lundin <daniel.lundin@scilifelab.se>
2013-02-14 21:11:17 +0100

    Trying to handle taxa that are not lookupable

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -48,6 +48,10 @@ class Taxon < AnnotationTarget
   def lookup!
     if source_db == 'NCBI' and ncbi_taxon_id = source_identifier.to_i
       hierarchy = BiosqlWeb.ncbi_taxon_id2full_taxon_hierarchy(ncbi_taxon_id)
+      if ( not hierarchy ) or hierarchy.length == 0 or not hierarchy[0]['scientific_name']
+	logger.warn "Couldn't lookup hierarchy for NCBI taxon id: #{ncbi_taxon_id}, hierarchy: #{hierarchy ? hierarchy.inspect : "hierarchy nil"}"
+	return
+      end
       self.name ||= hierarchy[0]['scientific_name']
       self.rank = hierarchy[0]['node_rank']
       hierarchy.each do |tentry|
