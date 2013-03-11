c96d1410e258c7cec5d782b95bd46032bdec793d
Daniel Lundin <daniel.lundin@scilifelab.se>
2013-03-11 17:20:06 +0100

    Turned phylum select into left join -- didn't change anything

--- db/sql/select_phylum_seed_leaf_cdna_observation.sql
+++ db/sql/select_phylum_seed_leaf_cdna_observation.sql
@@ -6,9 +6,9 @@ SELECT
   sum(cdna.n_specific)
 FROM
   samples s JOIN
-  cdna_observations cdna ON s.id = cdna.sample_id JOIN
+  cdna_observations cdna ON s.id = cdna.sample_id LEFT JOIN
   cdna_observation_taxons cdna_t ON cdna.id = cdna_t.cdna_observation_id JOIN
-  taxons t ON t.id = cdna_t.taxon_id JOIN
+  taxons t ON t.id = cdna_t.taxon_id LEFT JOIN
   cdna_observation_functions cdna_f ON cdna.id = cdna_f.cdna_observation_id JOIN
   functions f ON f.id = cdna_f.function_id
 WHERE
@@ -19,6 +19,7 @@ GROUP BY
   t.phylum,
   f.name
 ORDER BY
+  s.code,
   f.name,
   t.domain,
   t.phylum
